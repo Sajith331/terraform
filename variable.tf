614e1bbf998147b02a196ca64ec4380f4eaf937c
root <root@ip-172-31-37-186.ec2.internal>
2024-01-22 10:32:39 +0000

    ec2 instance variable file

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "iname" {
+type = string
+default= "ms"
+}
+variable "iami" {
+type= string
+default= "ami-0c0b74d29acd0cd97"
+}
+
+variable "itype" {
+type =string
+default = "t2.micro"
+}
+variable "ivolume" {
+type = number
+default = 14
+}
+
+variable "izone" {
+type= string
+default= "us-east-1a"
+}
